fa0a10ddcf5ea47cd7a17263a38367a5b74553c8
anamolhasan <mdanamolhasan1998@gmail.com>
2026-01-05 01:04:23 +0600

    part 1 complited

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import { router } from './router/router'
 import { RouterProvider } from 'react-router'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,17 @@ import Banner from './components/Banner'
 import Services from './components/service/Service'
 import ClientLogosMarquee from './components/ClientLogosMarquee'
 
+import Benefits from './components/Benefits/Benefits'
+import BeMerchant from './components/BeMerchant'
+
 const Home = () => {
   return (
     <div>
       <Banner />
       <Services />
       <ClientLogosMarquee />
+      <Benefits />
+      <BeMerchant />
     </div>
   )
 }
